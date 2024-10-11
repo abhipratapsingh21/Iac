fe24c30d1ac08f143bb228c675cd139b504e2e0b
abhipratapsingh21 <abhipratapsingh.2109@gmail.com>
2024-10-11 18:31:03 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,76 @@
+provider "azurerm" {
+  features {}
+  subscription_id = "2051b268-d4b9-4ebc-ab00-4e2fbb3d2749"
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "demo-rg"
+  location = "East US"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "demo-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "frontend-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "backend" {
+  name                 = "backend-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_subnet" "database" {
+  name                 = "database-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.3.0/24"]
+}
+
+resource "azurerm_network_security_group" "frontend_nsg" {
+  name                = "frontend-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+resource "azurerm_network_security_rule" "allow_http" {
+  name                        = "Allow-HTTP"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "80"
+  source_address_prefixes     = ["203.0.113.5", "198.51.100.10"]  # Add your allowed IP addresses here
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.frontend_nsg.name
+}
+
+resource "azurerm_network_security_rule" "deny_all" {
+  name                        = "Deny-All"
+  priority                    = 200
+  direction                   = "Inbound"
+  access                      = "Deny"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.frontend_nsg.name
+}
+
+resource "azurerm_subnet_network_security_group_association" "frontend_association" {
+  subnet_id                 = azurerm_subnet.frontend.id
+  network_security_group_id = azurerm_network_security_group.frontend_nsg.id
+}
